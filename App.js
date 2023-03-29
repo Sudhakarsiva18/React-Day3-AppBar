78914c0ae4aed88196ced3992c144c193b4ecdb9
Sudhakarsiva18 <108916787+Sudhakarsiva18@users.noreply.github.com>
2023-03-29 10:10:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import ButtonAppBar from './day3/Appbar'
+import Image from './day3/Image'
+
+function App() {
+  return (
+    <div>
+      <ButtonAppBar/>
+      <br/>
+      <Image/>
+    </div>
+  )
+}
+
+export default App
---
+++ Image.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import msd from './MSD.jpg'
+
+function Image() {
+  return (
+    <div className='div1'>
+        <center>
+            <img src={msd} width="700px" height="500px" alt=''></img>
+        </center>
+    </div>
+  )
+}
+
+export default Image
